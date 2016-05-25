1047348efcc95df78a70f259d68f79cb89065cf6
Olivier Delmotte <olivier@odelmotte.fr>
2016-05-25 11:09:58 +0200

    0.0.46 - Tar.Gz

--- ode-task-lib/tasks/Write-TarGz/Write-TarGz.ps1
+++ ode-task-lib/tasks/Write-TarGz/Write-TarGz.ps1
@@ -1,8 +1,7 @@
 [CmdletBinding()]
 param(
 	[string] $TargetFileName,
-	[string] $SourceFolder,
-	[string] $FilePath
+	[string] $SourceFolder
 )
 
 Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
@@ -11,34 +10,20 @@ Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 function Write-TarGz {
     param (
 		[string] $TargetFileName,
-		[string] $SourceFolder,
-		[string] $FilePath
+		[string] $SourceFolder
     )
-
-	#($FilePath -split '[\r\n]').Trim() | % {
-	#	if([System.IO.Path]::IsPathRooted($_))
-	#	{
-	#		$Files += $_
-	#	}
-	#	else
-	#	{
-	#		$Files += Join-Path $SourceFolder $_
-	#	}
-	#}
-	$Files = ($FilePath -split '[\r\n]').Trim()
 
 	Write-Verbose "TargetFileName        : $TargetFileName"
 	Write-Verbose "SourceFolder          : $SourceFolder"
-	Write-Verbose "FilePath              : $FilePath"
-	Write-Verbose "Files                 : $Files"
 
 	$TargetFileName = $TargetFileName -ireplace ".tar.gz"
 	New-Alias -Name 7z -Value (Join-Path $PSScriptRoot "7za.exe")
 
 	$generatedFile = (Join-Path $SourceFolder "$TargetFileName.tar")
-	& 7z a -ttar "$generatedFile" "$(Join-Path $SourceFolder "*.*")" -r #$Files
+	& 7z a -ttar "$generatedFile" "$(Join-Path $SourceFolder "*.*")" -r
 	$generatedFile2 = "$generatedFile.gz"
     & 7z a -tgzip "$generatedFile2" "$generatedFile"
+	Remove-Item $generatedFile -Force
 }
 
 Write-TarGz -TargetFileName $TargetFileName -SourceFolder $SourceFolder -FilePath $FilePath
